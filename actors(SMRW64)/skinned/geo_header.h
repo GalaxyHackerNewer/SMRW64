d5beec905732583dd386f7a3150fb142c743d02e
galaxyhacker newer <72502880+GalaxyHackerNewer@users.noreply.github.com>
2024-09-19 17:22:35 +0200

    Add files via upload

---
+++ actors(SMRW64)/skinned/geo_header.h
@@ -0,0 +1,8 @@
+extern const GeoLayout skinned_geo[];
+extern u8 skinned_treasure_chest_side_rgba16_rgba16[];
+extern Vtx skinned_skinned_mesh_layer_1_vtx_cull[8];
+extern Vtx skinned_skinned_mesh_layer_1_vtx_0[163];
+extern Gfx skinned_skinned_mesh_layer_1_tri_0[];
+extern Gfx mat_skinned_f3dlite_material[];
+extern Gfx mat_revert_skinned_f3dlite_material[];
+extern Gfx skinned_skinned_mesh_layer_1[];
